f523bf65df903e7f31843fde42ab560c0ad8115f
fortinjoffrey <fortinjoffrey@gmail.com>
2021-10-28 10:09:39 +0200

    Present and prewarm flutter in android app

---
+++ AndroidApp/app/src/main/java/com/example/androidapp/App.kt
@@ -0,0 +1,41 @@
+package com.example.androidapp
+
+import android.app.Application
+import android.content.Context
+import io.flutter.FlutterInjector
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
+
+const val FLUTTER_ENGINE_ID = "flutter_engine_id"
+const val FLUTTER_METHOD_CHANNEL_ID = "flutter_method_channel_id"
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // prewarm flutter engine with app auth engine id
+        prewarmFlutterEngine(applicationContext, FLUTTER_ENGINE_ID, "main")
+    }
+
+    private fun prewarmFlutterEngine(context: Context, engineId: String, entrypoint: String) {
+        if (!FlutterEngineCache.getInstance().contains(engineId)) {
+            // Instantiate a FlutterEngine
+            val flutterEngine = FlutterEngine(context)
+
+            // Create a general method channel
+            MethodChannel(flutterEngine.dartExecutor.binaryMessenger,FLUTTER_METHOD_CHANNEL_ID)
+
+            // Start executing Dart code to pre-warm the FlutterEngine.
+            flutterEngine.dartExecutor.executeDartEntrypoint(
+                DartExecutor.DartEntrypoint(
+                    FlutterInjector.instance().flutterLoader().findAppBundlePath(), entrypoint
+                )
+            )
+
+            // Cache the FlutterEngine to be used by FlutterActivity.
+            FlutterEngineCache.getInstance().put(engineId, flutterEngine)
+        }
+    }
+}
--- AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngineCache
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstButton = findViewById<Button>(R.id.button)
+
+        firstButton.setOnClickListener {
+            if (FlutterEngineCache.getInstance().contains(FLUTTER_ENGINE_ID)) {
+                startActivity(
+                    FlutterActivity.withCachedEngine(FLUTTER_ENGINE_ID)
+                        .build(applicationContext)
+                )
+            }
+        }
     }
 }
